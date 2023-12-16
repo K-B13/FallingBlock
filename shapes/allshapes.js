9468b18211296ad35f44cc3d1a547ebe6f4e809e
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-12-16 16:57:27 +0300

    Add z shape

--- shapes/allshapes.js
+++ shapes/allshapes.js
@@ -3,5 +3,6 @@ import { oPiece } from './oshape.js'
 import { tPiece } from './tshape.js'
 import { lPiece } from './lshape.js'
 import { jPiece } from './jshape.js'
+import { zPiece } from './zshape.js'
 
-export { iPiece, oPiece, tPiece, lPiece, jPiece }
+export { iPiece, oPiece, tPiece, lPiece, jPiece, zPiece }
